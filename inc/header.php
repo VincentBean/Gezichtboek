15c15cbacb8401d3571e1602024fc8c0eb88c305
Vincent Boon <Vincent1468@gmail.com>
2015-02-11 12:59:09 +0100

    Voer style.php en header.php toe.

---
+++ inc/header.php
@@ -0,0 +1,30 @@
+<?php
+
+	echo "<!-- Navigation -->
+    <nav class='navbar navbar-default navbar-fixed-top' style='background-color: black; height: 5%; padding: 0;'>
+        <div class='container'>
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class='navbar-header '>
+                <a class='navbar-brand' href='index.php'>Gezichtboek</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class='collapse navbar-collapse' id='bs-example-navbar-collapse-1'>
+                <ul class='nav navbar-nav navbar-right'>
+                    <li>
+                        <a href='home.php'>Tijdlijn</a>
+                    </li>
+                    <li>
+                        <a href='chat.php'>Chat</a>
+                    </li>
+                    <li>
+                        <a href='profiel.php'>Mijn profiel</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>";
+
+?>
--- index.php
+++ index.php
@@ -21,35 +21,18 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="description" content="Gezichtboek">
+    <meta name="author" content="Gezichtboek">
 
     <title>Gezichtboek</title>
-
-    <!-- Bootstrap Core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom CSS -->
-    <link href="css/agency.css" rel="stylesheet">
 
     <!-- Onze CSS -->
     <link rel="stylesheet" type="text/css" href="css/index.css">
 
-    <!-- Custom Fonts -->
-    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-    <link href="http://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
-    <link href='http://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
+    <?php
+        include "inc/style.php";
+    ?>
 
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
-    <link rel="shortcut icon" type="image/png" href="img/favicon.png">
 
 </head>
 
